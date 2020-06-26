6242282428985bfd1eeadd4410ed35cbc4672c1b
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2020-06-25 22:44:07 -0400

    implementation of create a Transaction on Services

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('This type of transaction is not valid');
+    }
+
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+
+      if (value > balance.total) {
+        throw Error("Haven't no balance to create this transaction.");
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
