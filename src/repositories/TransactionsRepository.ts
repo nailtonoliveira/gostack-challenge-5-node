1858c9ab4b34d0a7e029396f04d1bf7d70288e75
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2020-06-25 22:43:28 -0400

    implementation of get balance based on transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -24,7 +24,21 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') balance.income += transaction.value;
+
+      if (transaction.type === 'outcome') balance.outcome += transaction.value;
+    });
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
   public create({ title, value, type }: CreateTransactionDTO): Transaction {
